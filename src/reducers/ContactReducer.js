cb4972a74e83a7c5170889eb6f4e494f17f4b122
aizadazh <46402847+aizadazh@users.noreply.github.com>
2019-08-06 13:31:21 +0600

    actions and reducers are done

---
+++ src/reducers/ContactReducer.js
@@ -0,0 +1,42 @@
+import {
+    GET_CONTACTS,
+    GET_CONTACT,
+    DELETE_CONTACT,
+    ADD_CONTACT,
+    EDIT_CONTACT,
+} from "../actions/ActionTypes";
+
+const initialState = {
+    contacts: [],
+    contact: {},
+};
+export default function (state=initialState, action) {
+    switch (action.type){
+        case GET_CONTACTS:
+            return { ...state, contacts: action.payload };
+        case GET_CONTACT:
+            return { ...state, contact: action.payload }; 
+        case ADD_CONTACT: {
+            return { ...state, contacts: [ ...state.contacts, action.payload ] };            
+        } 
+        case EDIT_CONTACT: {
+            return {
+                ...state,
+                contacts: state.contacts.map(contact => {
+                    if(contact.id === action.payload.id) return action.payload;
+                    else return contact;
+                }),
+            };
+        }
+        case DELETE_CONTACT: {
+            return {
+                ...state,
+                contacts: state.contact.filter(
+                    contact => contact.id !== action.payload,
+                ),
+            };
+        }
+        default:
+            return state;
+        }
+    }
