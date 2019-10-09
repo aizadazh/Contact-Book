7630d42e0e2dbd6f8c948fd18afa0006a730e0f7
aizadazh <46402847+aizadazh@users.noreply.github.com>
2019-10-09 07:04:58 +0600

    Updates

---
+++ precache-manifest.d855f9b77d3a3985a1167598eaaa228f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1b3ae7bd14a121d6ecab8b2c8e2800",
+    "url": "/contact-book/index.html"
+  },
+  {
+    "revision": "6c7476d603fcb9a84f28",
+    "url": "/contact-book/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "9ae21972e1723fa08258",
+    "url": "/contact-book/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "6c7476d603fcb9a84f28",
+    "url": "/contact-book/static/js/2.3a5bb2eb.chunk.js"
+  },
+  {
+    "revision": "9ae21972e1723fa08258",
+    "url": "/contact-book/static/js/main.4da5cbd4.chunk.js"
+  },
+  {
+    "revision": "ae949437e2ac772d987a",
+    "url": "/contact-book/static/js/runtime~main.fb4e6df1.js"
+  }
+]);
